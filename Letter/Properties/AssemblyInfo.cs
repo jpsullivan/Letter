145220a4179a466ac30ea0d124ced8d83adc5c3e
Josh Sullivan <jaysche.sullivan@gmail.com>
2015-05-28 01:24:39 -0400

    Prepping for test release

--- Letter/Properties/AssemblyInfo.cs
+++ Letter/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Letter")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A customizable email building/sending library for .NET projects.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Josh Sullivan")]
 [assembly: AssemblyProduct("Letter")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
